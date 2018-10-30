d207416260fcae4673de8b02a32aff48ae1c6634
David Miotti <david@muxumuxu.com>
2018-10-30 14:08:51 +0100

    [+] Added scroll-behavior on route change to detect # correctly

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -48,7 +48,41 @@ module.exports = {
   serverMiddleware: [morgan('combined', { stream: accessLogStream })],
 
   router: {
-    middleware: ['i18n', 'contentful']
+    middleware: ['i18n', 'contentful'],
+    scrollBehavior(to, _from, savedPosition) {
+      // if the returned position is falsy or an empty object,
+      // will retain current scroll position.
+      let position = false
+
+      // if no children detected
+      if (to.matched.length < 2) {
+        // scroll to the top of the page
+        position = { x: 0, y: 0 }
+      } else if (
+        to.matched.some(r => r.components.default.options.scrollToTop)
+      ) {
+        // if one of the children has scrollToTop option set to true
+        position = { x: 0, y: 0 }
+      }
+
+      // savedPosition is only available for popstate navigations (back button)
+      if (savedPosition) {
+        position = savedPosition
+      }
+
+      return new Promise(resolve => {
+        // wait for the out transition to complete (if necessary)
+        window.$nuxt.$once('triggerScroll', () => {
+          // coords will be used if no selector is provided,
+          // or if the selector didn't match any element.
+          if (to.hash && document.querySelector(to.hash)) {
+            // scroll to anchor by returning the selector
+            position = { selector: to.hash }
+          }
+          resolve(position)
+        })
+      })
+    }
   },
 
   loading: { color: '#3B8070' },

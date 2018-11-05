3b3e582cc137c46ffb076893f93619678796c095
Quentin Bellanger <hello@quentin-bellanger.com>
2018-11-05 11:22:36 +0100

    [+] Size site layout & add outline on keyboard event

--- template/layouts/default.vue
+++ template/layouts/default.vue
@@ -8,18 +8,34 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    handleKeyboardUser(e) {
+      if (e.keyCode === 9) {
+        window.removeEventListener('keyup', this.handleKeyboardUser)
+        const classes = document.documentElement.classList
+        classes.add('with-keyboard')
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('keyup', this.handleKeyboardUser)
+  }
+}
 </script>
 
 <style lang="sass" scoped>
 .site
   background: $white
+  margin: 0 auto
+  max-width: 1440px
+  overflow: hidden
 
   [href="#main"]
-    left: 5px
+    left: 10px
     position: absolute
     top: -100%
 
     &:focus
-      top: 5px
+      top: 10px
 </style>
--- template/pages/_lang/index.vue
+++ template/pages/_lang/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <h1>Welcome to {{ name }}</h1>
+    <p>Learn more how to use this template by reading <a href="https://github.com/muxumuxu/starter-template">the documentation</a>.</p>
   </section>
 </template>
 

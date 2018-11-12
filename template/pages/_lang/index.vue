025dcb5b518730fa782e8d0af7aace054dda476c
Quentin Bellanger <hello@quentin-bellanger.com>
2018-11-12 11:59:38 +0100

    [-] Fix scss ending }

--- template/pages/_lang/index.vue
+++ template/pages/_lang/index.vue
@@ -1,7 +1,6 @@
 <template>
   <section>
     <h1>Welcome to {{ name }}</h1>
-    <p>Learn more how to use this template by reading <a href="https://github.com/muxumuxu/starter-template">the documentation</a>.</p>
   </section>
 </template>
 
